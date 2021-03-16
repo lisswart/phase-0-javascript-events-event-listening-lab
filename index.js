ccce81bd1f87b2dc4750f030d0e53c1b10732081
lisswart <arnoudlisa@gmail.com>
2021-03-16 19:12:57 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const input = document.getElementById('input');
+
+input.addEventListener('click', function() 
+                                {alert('I was clicked!')}
+                        );
+
 function addingEventListener() {
+    alert('I was clicked!');
+    input.addEventListener('click', addingEventListener);
 }
+
